203b9816591cf2a233b70fb4e6b6e30f2b6f03c2
Mike MacDonald <mike.macdonald@active.com>
2012-04-18 17:13:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskman::Application.config.secret_token = '80a2e84da239ec9777bdde9b543a6b458068e9380a390502df9bdd3bd3c114caf28141b0bb0c477e21cc19c34670cdea0dca49acaea0f6fadb3d27f7a8c9d4a4'
